b7dbaa484aaaa029f5ed8f1fea32842b61b0be23
Jen Kelly <jen.kelly@powerhrg.com>
2025-05-20 10:24:53 -0400

    Retrieve user data and print to console

--- index.js
+++ index.js
@@ -10,4 +10,25 @@ function submitData(name, email) {
       email: `${email}`,
     }),
   })
+    .then((res) => res.json())
+    .then((data) => {
+      const user = data
+      console.log(user.id)
+    })
+    .catch((error) => {
+      alert("Bad things! Ragnarők!")
+      console.log(error.message)
+    })
 }
+
+submitData()
+
+const element = document.createElement("div")
+const ul = document.createElement("ul")
+
+document.body.appendChild(element)
+element.appendChild(ul)
+
+//  1) submitData()
+//        handles the POST request response, retrieves the new id value and appends it to the DOM:
+//      AssertionError: expected '\n    <script src="index.js"></script>\n  \n\n' to include 95
